a6b14ee457a1aafc48b4b9f6b871b757f06a5415
wrojas <welectronic@gmail.com>
2025-01-05 12:06:50 -0500

    Base Project

---
+++ src/main/java/lite/thinking/taller01/Taller01Application.java
@@ -0,0 +1,13 @@
+package lite.thinking.taller01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Taller01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Taller01Application.class, args);
+	}
+
+}
---
+++ src/test/java/lite/thinking/taller01/Taller01ApplicationTests.java
@@ -0,0 +1,13 @@
+package lite.thinking.taller01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Taller01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
